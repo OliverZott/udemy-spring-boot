d4b12e6f114657dd0d288c792af10f12b49614ec
Oliver Zott <zott_oliver@web.de>
2021-12-03 21:47:31 +0100

    Redirect added.

--- src/main/java/org/velosaurus/demo/springbootdemo/controller/EmployeeController.java
+++ src/main/java/org/velosaurus/demo/springbootdemo/controller/EmployeeController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.velosaurus.demo.springbootdemo.entity.Employee;
 import org.velosaurus.demo.springbootdemo.service.EmployeeService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -20,7 +22,7 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/list")
     public String listEmployees(Model model) {
         List<Employee> employees = employeeService.getEmployees();
         model.addAttribute("employees", employees);
