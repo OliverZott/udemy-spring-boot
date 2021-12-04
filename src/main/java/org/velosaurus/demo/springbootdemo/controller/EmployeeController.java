016d9643b7040ee0bf77890139a78e33004247d2
Oliver Zott <zott_oliver@web.de>
2021-12-04 11:43:50 +0100

    Form component added.

--- src/main/java/org/velosaurus/demo/springbootdemo/controller/EmployeeController.java
+++ src/main/java/org/velosaurus/demo/springbootdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.velosaurus.demo.springbootdemo.entity.Employee;
 import org.velosaurus.demo.springbootdemo.service.EmployeeService;
@@ -27,6 +29,24 @@ public class EmployeeController {
         List<Employee> employees = employeeService.getEmployees();
         model.addAttribute("employees", employees);
         return "list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        // model attribute to bind form data
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+        return "form-for-add";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+
+        // Post/Redirect/Get Pattern implementation
+        return "redirect:/employees/list";
     }
 
 }
